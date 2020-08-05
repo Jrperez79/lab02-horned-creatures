ab845dffcc13b3e7ca0b33e1640f0e457ac598c9
Jrperez79 <joelrperez@gmail.com>
2020-08-05 08:17:15 -0700

    updated CSS and CreatureFilter.js

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import ImageItem from './ImageItem.js'
 
 export default class ImageList extends Component {
     render() {
         return (
             <div>
-                
+                <ul id="creature-list">
+                    { this.props.images.map(creature => (
+                        <ImageItem key={ creature.title } src={ creature } />
+                    ))}
+                </ul>
             </div>
         )
     }
